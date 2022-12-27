b9c749a263930d4242ab70b151c3cce0c30fa79b
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-12-27 22:14:25 +0100

    init

--- src/main/webapp/app/entities/ponudjaci/update/ponudjaci-update.component.ts
+++ src/main/webapp/app/entities/ponudjaci/update/ponudjaci-update.component.ts
@@ -11,6 +11,7 @@ import { PonudjaciService } from '../service/ponudjaci.service';
 @Component({
   selector: 'jhi-ponudjaci-update',
   templateUrl: './ponudjaci-update.component.html',
+  styleUrls: ['./ponudjaci.scss'],
 })
 export class PonudjaciUpdateComponent implements OnInit {
   isSaving = false;
